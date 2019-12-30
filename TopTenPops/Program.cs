2eaa490bcee8d75f756ecf19ad767ed7ef747d7d
Outmane Baladi <outmanebaladi22@gmail.com>
2019-12-30 11:48:04 +0100

    importing from a CVS data source into an array

---
+++ TopTenPops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TopTenPops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Outmane.BALADI\source\repos\TopTenPops\TopTenPops\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach(var country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
