89d3124dec5e2d95324ac398d11726db26c1db7a
Outmane Baladi <outmanebaladi22@gmail.com>
2019-12-31 11:58:31 +0100

    working with Dictionary collection

--- TopTenPops/CsvReader.cs
+++ TopTenPops/CsvReader.cs
@@ -13,9 +13,9 @@ namespace TopTenPops
             this._csvFilePath = csvFilePath;
         }
 
-        public List<Country> ReadAllCountries()
+        public Dictionary<string, Country> ReadAllCountries()
         {
-            List<Country> countries = new List<Country>();
+            Dictionary<string, Country> countries = new Dictionary<string, Country>();
 
             using(StreamReader sr = new StreamReader(_csvFilePath))
             {
@@ -24,8 +24,8 @@ namespace TopTenPops
                 string csvLine;
                 while((csvLine = sr.ReadLine()) != null)
                 {
-                    
-                    countries.Add(ReadCountryFromCsvLine(csvLine));
+                    Country country = ReadCountryFromCsvLine(csvLine);
+                    countries.Add(country.Code, country);
                 }
             }
 
--- TopTenPops/Program.cs
+++ TopTenPops/Program.cs
@@ -10,17 +10,22 @@ namespace TopTenPops
             string filePath = @"C:\Users\Outmane.BALADI\source\repos\TopTenPops\TopTenPops\Pop by Largest Final.csv";
             CsvReader reader = new CsvReader(filePath);
 
-            List<Country> countries = reader.ReadAllCountries();
-            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
-            int lilliputIndex = countries.FindIndex(c => c.Population < 2_000_000);
-            countries.Insert(lilliputIndex, lilliput);
-            countries.RemoveAt(lilliputIndex);
+            Dictionary<string, Country> countries = reader.ReadAllCountries();
 
-            foreach(var country in countries)
+            Console.WriteLine("Which country code do you want to look up?");
+            string userInput = Console.ReadLine();
+
+            bool gotCountry = countries.TryGetValue(userInput, out Country country);
+            if (!gotCountry)
             {
-                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+                Console.WriteLine($"Sorry, there is no country with code, {userInput}");
             }
-            Console.WriteLine($"{countries.Count} countries");
+            else
+            {
+                Console.WriteLine($"{country.Name} has population {PopulationFormatter.FormatPopulation(country.Population)}");
+            }
+
+
         }
     }
 }
